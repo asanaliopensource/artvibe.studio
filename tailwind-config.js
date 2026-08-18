986403afb460a9c3eeecaef5268e101ccc00e476
asanaliopensource <asulan.masanov@gmail.com>
2026-08-18 17:03:09 +0500

    Add Tailwind CSS configuration with custom theme

---
+++ tailwind-config.js
@@ -0,0 +1,23 @@
+        tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    colors: {
+                        brand: {
+                            50: '#f0f5ff',
+                            100: '#e5edff',
+                            500: '#6366f1',
+                            600: '#4f46e5',
+                            700: '#4338ca',
+                            900: '#1e1b4b',
+                        },
+                        darkbg: '#0f1117',
+                        panelbg: 'rgba(22, 26, 36, 0.75)',
+                    },
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                        mono: ['Fira Code', 'monospace'],
+                    }
+                }
+            }
+        }
